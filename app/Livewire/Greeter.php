cb19c7a8a0f75d880725e085765b807254ccf3f6
Edwin Orellana <educsis@gmail.com>
2025-03-22 22:56:23 -0600

    Initial commit

---
+++ app/Livewire/Greeter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Greeter extends Component
+{
+    public $name = "Edwin Orellana";
+
+    public function changeName($newName)
+    {
+        $this->name = $newName;
+    }
+    public function render()
+    {
+        return view('livewire.greeter');
+    }
+}
---
+++ resources/views/livewire/greeter.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <div>
+        Hello {{ $name }}
+    </div>
+
+    <div class="mt-2">
+        <button class="text-white font-medium rounded-md px-4 py-2 bg-blue-600"
+        wire:click="changeName('edwinsito Orellana')"
+        >
+            Greet me
+        </button>
+    </div>
+</div>
