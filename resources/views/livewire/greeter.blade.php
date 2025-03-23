c691877ea209f844762bad7eb60de5db8534c3d5
Edwin Orellana <educsis@gmail.com>
2025-03-22 22:59:29 -0600

    Your commit message here

--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -3,10 +3,12 @@
         Hello {{ $name }}
     </div>
 
+    <div>
+        <input type="text" id="newName">
+    </div>
+
     <div class="mt-2">
-        <button class="text-white font-medium rounded-md px-4 py-2 bg-blue-600"
-        wire:click="changeName('edwinsito Orellana')"
-        >
+        <button wire:click="changeName(document.querySelector('#newName').value)">
             Greet me
         </button>
     </div>
